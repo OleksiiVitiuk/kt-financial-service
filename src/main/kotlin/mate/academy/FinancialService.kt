603b0d4f067149eb6287d2a664dfe582ec9eff6b
Oleksii Vitiuk <vitiuk.oleksii@gmail.com>
2025-11-15 22:35:03 +0200

    done

--- src/main/kotlin/mate/academy/FinancialService.kt
+++ src/main/kotlin/mate/academy/FinancialService.kt
@@ -1,14 +1,49 @@
 package mate.academy
 
+@JvmInline
+value class CurrencyAmount(val amount: Double) {
+    init {
+        require(amount >= 0) { "Amount must be non-negative" }
+    }
+}
+
+@JvmInline
+value class CurrencyCode(val code: String) {
+    init {
+        require(code.matches(Regex("[A-Z]{3}"))) {
+            "Currency code must be 3 uppercase letters"
+        }
+    }
+}
+
+@JvmInline
+value class AccountNumber(val number: String) {
+    init {
+        require(number.matches(Regex("\\d{10}"))) {
+            "Account number must be a 10-digit string"
+        }
+    }
+}
+
+@JvmInline
+value class TransactionId(val id: String) {
+    init {
+        require(id.isNotBlank()) { "Transaction ID cannot be empty" }
+    }
+}
+
 class FinancialService {
+
     fun transferFunds(
         source: AccountNumber,
         destination: AccountNumber,
         amount: CurrencyAmount,
         currencyCode: CurrencyCode,
         transactionId: TransactionId
-    ) : String {
-        // TODO: implement
+    ): String {
+        return "Transferred ${amount.amount} ${currencyCode.code} " +
+                "from ${source.number} to ${destination.number}. " +
+                "Transaction ID: ${transactionId.id}"
     }
 
     fun convertCurrency(
@@ -16,11 +51,12 @@ class FinancialService {
         fromCurrency: CurrencyCode,
         toCurrency: CurrencyCode
     ): CurrencyAmount {
-        // TODO: implement
+        val rate = getExchangeRate(fromCurrency, toCurrency)
+        val converted = amount.amount * rate
+        return CurrencyAmount(converted)
     }
 
     private fun getExchangeRate(fromCurrency: CurrencyCode, toCurrency: CurrencyCode): Double {
-        // Placeholder exchange rate - in a real application, you'd fetch this from a financial API
         return when {
             fromCurrency.code == "USD" && toCurrency.code == "EUR" -> 0.93
             fromCurrency.code == "USD" && toCurrency.code == "GBP" -> 0.82
